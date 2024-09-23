8b0118a1121ba7ea00a92170bf83526783edb390
desktop <jallen108@gsu.edu>
2024-09-23 14:06:38 -0400

    simplify link builder since you trigger it before you build the links

--- R/utils.R
+++ R/utils.R
@@ -13,19 +13,12 @@ link_builder = \(country, ecd_version){
 
   country_names = country_names[country_names$name_in_dataset %in% country,]
 
-if(nrow(country_names) > 0){
   country_names = country_names |>
     within({
       file_names = glue::glue('https://github.com/joshuafayallen/executivestatements/releases/download/{ecd_version}/{file_name}.parquet')
     })
     country_names = country_names$file_names
-}
-  else{
-  
-  cli::cli_abort('Country is not in dataset')
-  
-}
-  
+
   
   
  
--- tests/testthat/test-link_builder.R
+++ tests/testthat/test-link_builder.R
@@ -8,6 +8,16 @@ test_that('checking the link building function',
 objs = link_builder(country = 'United States of America', ecd_version = '1.0.0')
   
 expect_equivalent(objs, 'https://github.com/joshuafayallen/executivestatements/releases/download/1.0.0/united_states_of_america.parquet')
-
+  
+  
+obs_two = link_builder(country = 'Argentina, Czechia, Turkey', ecd_version = '1.0.0')
+  
+link_vec = c('https://github.com/joshuafayallen/executivestatements/releases/download/1.0.0/turkey.parquet',
+             'https://github.com/joshuafayallen/executivestatements/releases/download/1.0.0/czechia.parquet',
+            'https://github.com/joshuafayallen/executivestatements/releases/download/1.0.0/argentina.parquet')
+  
+expect_equivalent(obs_two, link_vec)  
+  
+  
 
 })
