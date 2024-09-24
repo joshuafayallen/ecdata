b595cd0f33dfe0c777fe54a04fddaf997bec0ed8
desktop <jallen108@gsu.edu>
2024-09-24 17:02:09 -0400

    change dependencies to use vctrs::list_unchop instead of list_rbind to avoid bringing in all of purrr since arrow is already a huge dependency profile

--- R/load_ecd.R
+++ R/load_ecd.R
@@ -5,7 +5,7 @@
 #' @param country a character vector of country or countries in our dataset to download. for a list of valid names call `country_dictionary` 
 #' @param full_ecd to download the full ecd dataset set full_ecd to TRUE and don't provide an argument to the country argument
 #' @param ecd_version a character of ecd versions. 
-#' @importFrom purrr list_rbind
+#' @importFrom vctrs list_unchop
 #' 
 #' @examples
 #' \dontrun{
@@ -73,7 +73,7 @@ load_ecd = \(country = NULL, full_ecd = FALSE, ecd_version = '1.0.0'){
       ecd_data = lapply(link_to_read, \(x) arrow::read_parquet(x))
 
       ecd_data = ecd_data |>
-        list_rbind()
+        list_unchop()
 
       if(nrow(ecd_data) != 0){
         cli::cli_alert_success('Successfully downloaded data for {country}')
